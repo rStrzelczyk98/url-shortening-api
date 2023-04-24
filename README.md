38a0638cd9d48add9e3f86eb828f97d46769a365
Robert Strzelczyk <robertstrzelczyk98@gmail.com>
2023-04-24 10:42:49 +0200

    update

---
+++ README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - Shortly URL shortening API Challenge solution
+
+This is a solution to the [Shortly URL shortening API Challenge challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- Shorten any valid URL
+- See a list of their shortened links, even after refreshing the browser
+- Copy the shortened link to their clipboard in a single click
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+
+### Screenshot
+
+![](./images/final.jpg)
+
+### Links
+
+- Live Site URL: [url-shortening](https://rstrzelczyk98.github.io/url-shortening-api/)
+
+## Built with
+
+- HTML5
+- CSS3
+- JavaScript
+- shrtcode API
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@rStrzelczyk98](https://www.frontendmentor.io/profile/rStrzelczyk98)
--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 "use strict";
 const btn = document.querySelector(".submit");
-
 const menu_btn = document.querySelector(".hamburger");
+const input = document.getElementById("input");
+let linksArray = [];
 
+window.addEventListener("load", getLinks);
+input.addEventListener("focus", removeError);
 btn.addEventListener("click", function (e) {
-  const input = document.getElementById("input");
+  if (!input.value) {
+    displayError("Please add a link");
+    return;
+  }
   getURL(input.value);
 });
 menu_btn.addEventListener("click", displayMenu);
 
 async function getURL(link) {
-  const data = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`);
-  const url = await data.json();
-  urlCard(link, url.result.short_link);
-  // copy(url.result.short_link);
+  try {
+    const data = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`);
+    const url = await data.json();
+    if (url.ok) {
+      const url_object = { url: link, short_url: url.result.short_link };
+      linksArray.push(url_object);
+      localStorage.setItem("links", JSON.stringify(linksArray));
+      urlCard(link, url.result.short_link);
+    } else {
+      throw new Error(url.error);
+    }
+  } catch (error) {
+    displayError(error.message.split(",")[0]);
+  }
 }
 
 function copy(url) {
@@ -42,4 +58,24 @@ function urlCard(long, short) {
 function toggle(el) {
   el.classList.remove("active");
   el.textContent = "Copy";
+}
+
+function getLinks() {
+  if (!localStorage.getItem("links")) return;
+  const data = localStorage.getItem("links");
+  const array = JSON.parse(data);
+  linksArray = array;
+  linksArray.forEach((el) => urlCard(el.url, el.short_url));
+}
+function displayError(msg) {
+  const error = document.getElementById("error");
+  input.classList.add("error");
+  input.ariaInvalid = true;
+  error.textContent = msg;
+}
+function removeError() {
+  const error = document.getElementById("error");
+  input.classList.remove("error");
+  input.ariaInvalid = false;
+  error.textContent = "";
 }
